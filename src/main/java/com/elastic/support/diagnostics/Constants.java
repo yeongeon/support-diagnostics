d0181274cae640b360a03be7d406a3a4b6b4fa85
yeongeon <yeongeon@gmail.com>
2018-04-27 03:36:05 +0900

    add rules for ip, hostname

--- src/main/java/com/elastic/support/diagnostics/Constants.java
+++ src/main/java/com/elastic/support/diagnostics/Constants.java
@@ -20,4 +20,6 @@ public class Constants {
    public static final String logDirPattern = "*_log";
    public static final String logFilePattern = "[a-zA-Z0-9]+$(?!-\\d\\d\\d\\d-\\d\\d-\\d\\d$)*.log";
 
+   public static final String IP_REGEX = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+   public static final String HOSTNAME_REGEX = "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$";
 }
